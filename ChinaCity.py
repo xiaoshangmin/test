5516d5841153fc834c59129533837c4987a71aa5
xiaoshangmin <shangminxiao@gmail.com>
2016-07-04 21:07:29 +0800

    complete

--- ChinaCity.py
+++ ChinaCity.py
@@ -1,33 +1,35 @@
 # -*-coding:utf-8-*-
+"""
+
+"""
 __author__ = 'xiaoshangmin'
 from bs4 import BeautifulSoup
 import requests, pymysql, re, json, time
 
 DEBUG = False
-GSQL = True
-GJSON = False
+GJSON = True
 
-# 连接数据库
+# connect database
 def conn(localhost, user, password, database):
     db = pymysql.connect(localhost, user, password, database)
     db.set_charset('utf8')
     return db
 
 
-# 获取省级
+# get province
 def getprovince(style):
     # pinfo = province.find('tr').get_text(',', strip=True)
     return re.compile("#FFDF80").search(str(style))
 
 
-# 获取城市
+# get city
 def getcity(style):
     return re.compile("#CCFFFF").search(str(style)) or re.compile("#E6E6FA").search(str(style))
 
 
 def saveDataAsJson(val):
     with open('city.json', 'a', encoding='utf-8') as f:
-        f.write(json.dumps(val,ensure_ascii=False))
+        f.write(json.dumps(val))#ensure_ascii=False
 
 
 def saveData(db, *val):
@@ -46,13 +48,6 @@ def saveData(db, *val):
         db.commit()
     except:
         db.rollback()
-
-
-# def saveway(lists, db=None):
-#     if (db):
-#         saveData(db, *lists)
-#     else:
-#         saveDataAsJson(*lists)
 
 
 def save_area_sql(rs, db=None):
@@ -114,7 +109,6 @@ def save_area_json(rs):
                 for y in county:
                     yinfo = y.find_next().get_text()
                     if yinfo:
-                        # yinfos = [y, yinfo, cpaddr]
                         citydata[i]['child'][j]['child'].append({'code': y, 'name': yinfo, 'upaddr': cpaddr})
                 j = j + 1
 
@@ -125,7 +119,6 @@ def save_area_json(rs):
                 cinfo = c.find_next().get_text()
                 if cinfo == '县' or cinfo.isdigit():
                     continue
-                # data = [c, cinfo, upaddr]
                 citydata[i]['child'].append({'code': c, 'name': cinfo, 'upaddr': upaddr})
 
         i = i + 1
@@ -133,10 +126,8 @@ def save_area_json(rs):
 
 
 if __name__ == "__main__":
-    GSQL = False
+    GJSON = False
     start = time.clock()
-    if GSQL:
-        db = conn('localhost', 'root', '', 'test')
     # 维基百科地址
     urls = [
         'https://zh.wikipedia.org/wiki/%E4%B8%AD%E5%8D%8E%E4%BA%BA%E6%B0%91%E5%85%B1%E5%92%8C%E5%9B%BD%E8%A1%8C%E6%94%BF%E5%8C%BA%E5%88%92%E4%BB%A3%E7%A0%81_(1%E5%8C%BA)',
@@ -148,20 +139,25 @@ if __name__ == "__main__":
         'https://zh.wikipedia.org/wiki/%E4%B8%AD%E5%8D%8E%E4%BA%BA%E6%B0%91%E5%85%B1%E5%92%8C%E5%9B%BD%E8%A1%8C%E6%94%BF%E5%8C%BA%E5%88%92%E4%BB%A3%E7%A0%81_(7%E5%8C%BA)',
         'https://zh.wikipedia.org/wiki/%E4%B8%AD%E5%8D%8E%E4%BA%BA%E6%B0%91%E5%85%B1%E5%92%8C%E5%9B%BD%E8%A1%8C%E6%94%BF%E5%8C%BA%E5%88%92%E4%BB%A3%E7%A0%81_(8%E5%8C%BA)'
     ]
-    c = []
+    if GJSON:
+        c = []
+    else:
+        db = conn('localhost', 'root', '', 'test')
+
     for url in urls:
         r = requests.get(url)
         r.encoding = 'utf-8'
         bs = BeautifulSoup(r.content, 'html.parser')
         rs = bs.find_all('table', class_="wikitable")
-        if GSQL:
-            save_area_sql(rs, db)
-        else:
+        if GJSON:
             data = save_area_json(rs)
-            for n in range(0,len(data)):
+            for n in range(0, len(data)):
                 c.append(data[n])
-    if GSQL:
+        else:
+            save_area_sql(rs, db)
+    if GJSON:
         saveDataAsJson(c)
-    #db.close()
+    else:
+        db.close()
     end = time.clock()
     print('run time is %.03f seconds' % (end - start))
